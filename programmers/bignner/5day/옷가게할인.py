a61f627fe6efe515e8178410b0fad6cbaa7bf758
Dev-Gony <gwonyi190000@gmail.com>
2026-02-05 16:42:38 +0900

    5day

---
+++ programmers/bignner/5day/옷가게할인.py
@@ -0,0 +1,14 @@
+def solution(price):
+    answer = 0
+    if price >= 500000:
+        answer = int(price - (price * 0.2))
+    elif price >= 300000:
+        answer = int(price - (price * 0.1))
+    elif price >= 100000:
+        answer = int(price - (price *0.05))
+    else:
+        answer = int(price)
+    return answer
+
+
+# 뭐냐 왜 14점이나 오르냐 이거 개꿀
